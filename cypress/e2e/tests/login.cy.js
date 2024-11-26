804308070ee0e3940b1a60acf436e864842ee214
Vanja <vanjabrkans@gmail.com>
2024-11-26 12:55:56 +0100

    Login tests

--- cypress/e2e/tests/login.cy.js
+++ cypress/e2e/tests/login.cy.js
@@ -1,16 +1,40 @@
 import { navigateTo } from "../../support/pageObjects/navigation";
 import { onLogin } from '../../support/pageObjects/login';
+import users from '../../fixtures/users.json';
+import { onRegister } from '../../support/pageObjects/register';
 
-describe("Login page", () => {
-    // Run before each test: Navigate to the "Sign In" page
-    beforeEach('Open Application and navigate to Sign In page', () => {
+
+describe('Login Tests', () => {
+     // Run before each test: Navigate to the "Sign In" page
+     beforeEach('Open Application and navigate to Sign In page', () => {
         cy.visit('/') // Open the homepage
         navigateTo.goToSignIn(); // Navigate to the "Sign In" page
     });
 
-      // Test for valid data and successful registration
-  it('Should be able to login with valid credentials', () => {
-    onLogin.validateTheLoginPage(); // Validate the page
-    
-  });
+    const emailRegex = /^\S+@\S+\.\S+$/; // Regex to validate email format
+
+    users.forEach(user => {
+        it(`should ${user.alert 
+            ? 'show error message for invalid credentials' 
+            : user.success 
+            ? 'log in successfully' 
+            : user.email === ' ' && user.password === '' 
+            ? 'show error message for empty fields' 
+            : 'validate email format'} for ${user.email}`, () => {
+            onLogin.validateTheLoginPage(); // Validate the Login page
+            onLogin.login(user.email, user.password);
+
+            if (user.email === '' && user.password === '') {
+                onRegister.validateErrorMessageField('email-error', user.error);
+                onRegister.validateErrorMessageField('pass-error', user.error);
+            } else if (!emailRegex.test(user.email)) {
+                onRegister.validateErrorMessageField('email-error', user.error);
+            }  else if (user.alert) {
+                onLogin.validateInvalidCredentials(user.alert);
+            } else {
+                onLogin.validateValidCredentials(user.success);
+            }
+        });
+    });
 });
+
--- cypress/support/pageObjects/login.js
+++ cypress/support/pageObjects/login.js
@@ -1,5 +1,29 @@
+// Helper function to fill input fields
+// It clears the input field first and types the provided value only if it exists
+function fillInputField(fieldId, value) {
+    cy.get(`#${fieldId}`).clear(); // Clear the field to remove any existing text
+    if (value) { // Check if a value is provided
+        cy.get(`#${fieldId}`).type(value); // Type the value into the field
+    }
+}
 // Class to handle the Login page actions and validations
 export class LoginPage {
+
+        // Method to fill out the registration form and submit it
+        login(email, password) {
+            fillInputField('email', email); // Fill the First Name field
+            fillInputField('pass', password); // Fill the Last Name field
+            cy.get('button[type="submit"][id="send2"][class="action login primary"]').click(); // Click the Submit button
+        }
+
+    validateInvalidCredentials(expectedText) {
+        cy.get('div[role="alert"]').should('contain', expectedText);
+    }
+
+    validateValidCredentials(expectedText) {
+        cy.wait(3000);
+        cy.get('.header').find('span[class="logged-in"]').should('contain', expectedText);
+    }
     
     // Method to validate that the Login page is displayed correctly
     validateTheLoginPage() {
@@ -9,10 +33,12 @@ export class LoginPage {
         .and('contain', 'Customer Login');  // Verify the title contains the expected text
 
         // Validate that the "Registered Customers" heading is displayed
-        cy.get('strong[id="block-customer-login-heading"]')  // / Target the heading container by its role attribute
+        cy.get('.block-title > strong[id="block-customer-login-heading"]')  // Find the <strong> HTML element with the id "block-customer-login-heading"
         .should('have.text', 'Registered Customers'); // Verifies it contains the expected text
 
-       
+        // Validate that the "New Customers" heading is displayed
+        cy.get('strong[id="block-new-customer-heading"]')  // Find the <strong> HTML element with the id "block-new-customer-heading"
+        .should('have.text', 'New Customers'); // Verifies it contains the expected text
     }
 
    
